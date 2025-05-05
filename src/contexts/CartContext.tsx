95b50f7434ee60f39ac9dd39b4c025ca15a12f2d
Rodrigo Marques Tavares <rodrigour@gmail.com>
2025-05-05 17:29:14 -0300

    feat: Adiciona contexto de carrinho e provedor para gerenciamento de itens

---
+++ src/contexts/CartContext.tsx
@@ -0,0 +1,99 @@
+import { createContext, ReactNode, useState } from "react";
+import { ProductsProps } from "../pages/home/Home";
+
+interface CartContextData {
+  cart: CartProps[];
+  cartAmount: number;
+  addItemCart: (newItem: ProductsProps) => void;
+  removeItemCart: (product: CartProps) => void;
+  total: string;
+}
+
+interface CartProps {
+  id: string;
+  name: string;
+  code: string;
+  image: string;
+  price: number;
+  quantity: number;
+  total: number;
+}
+
+interface CartProviderProps {
+  children: ReactNode;
+}
+
+const CartContext = createContext({} as CartContextData);
+
+export const CartProvider = ({ children }: CartProviderProps) => {
+  const [cart, setCart] = useState<CartProps[]>([]);
+  const [total, setTotal] = useState("");
+
+  const addItemCart = (newItem: ProductsProps) => {
+    const indexItem = cart.findIndex((item) => item.id === newItem._id);
+
+    if (indexItem !== -1) {
+      let cartList = cart;
+      cartList[indexItem].quantity = cartList[indexItem].quantity + 1;
+      cartList[indexItem].total =
+        cartList[indexItem].price * cartList[indexItem].quantity;
+      setCart(cartList);
+      totalResultCart(cartList);
+      return;
+    }
+    let data = {
+      id: newItem._id,
+      name: newItem.name,
+      code: newItem.code,
+      image: newItem.image,
+      price: newItem.price,
+      quantity: 1,
+      total: newItem.price,
+    };
+    setCart((products) => [...products, data]);
+    totalResultCart([...cart, data]);
+  };
+
+  const removeItemCart = (product: CartProps) => {
+    const indexItem = cart.findIndex((item) => item.id === product.id);
+
+    if (cart[indexItem]?.quantity > 1) {
+      let cartList = cart;
+      cartList[indexItem].quantity = cartList[indexItem].quantity - 1;
+      cartList[indexItem].total =
+        cartList[indexItem].total - cartList[indexItem].price;
+      setCart(cartList);
+      totalResultCart(cartList);
+      return;
+    }
+    const removeItem = cart.filter((item) => item.id !== product.id);
+    setCart(removeItem);
+    totalResultCart(removeItem);
+  };
+
+  const totalResultCart = (items: CartProps[]) => {
+    let myCart = items;
+    let result = myCart.reduce((acc, obj) => {
+      return acc + obj.total;
+    }, 0);
+    const resultFormated = result.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+    setTotal(resultFormated);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        cartAmount: cart.length,
+        addItemCart,
+        removeItemCart,
+        total,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
