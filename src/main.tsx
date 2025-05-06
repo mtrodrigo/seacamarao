3221726f78d5de431b1594d856ee99fc4813023d
Rodrigo Marques Tavares <rodrigour@gmail.com>
2025-05-06 08:52:18 -0300

    feat: Adiciona suporte a react-hook-form e zod, atualiza estrutura de provedores

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import { CartProvider } from "./contexts/CartContext.tsx";
 import "./index.css";
 import App from "./App.tsx";
+import { UserProvider } from "./contexts/UserContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <UserProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </UserProvider>
   </StrictMode>
 );
