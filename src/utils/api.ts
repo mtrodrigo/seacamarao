1ed198785931e7a8429a620630c7842bfdb43514
Rodrigo Marques Tavares <rodrigour@gmail.com>
2025-05-08 18:16:28 -0300

    feat: Atualiza configuração do ambiente e adiciona tipos para Node.js

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const URL_API = process.env.URL_API
+
 export default axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: URL_API,
   headers: {
     "Content-Type": "application/json",
   },
